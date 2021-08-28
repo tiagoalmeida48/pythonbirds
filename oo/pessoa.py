1052d681a3475c107af85ff6d8fb66a1ee2316fc
tiagoalmeida48 <53452661+tiagoalmeida48@users.noreply.github.com>
2021-08-28 17:23:38 -0300

    Criado os atributos e valores

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=30):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Olá mundo'
 
 if __name__ == '__main__':
     p = Pessoa()
     print(p.cumprimentar())
+    p.nome = 'Tiago'
+    print(p.nome)
+    print(p.idade)
